2f0fc2a8ecdd3b3d65a7e719b335e673949c6939
Fthullh <fthullh99@gmail.com>
2021-05-29 22:20:49 +0800

    Project IDEA

---
+++ Module01/Module01_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Fathulloh";
+        String namaTengah = "Muhammad";
+        String namaBelakang = "Idham";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
