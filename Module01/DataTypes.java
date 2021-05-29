edb9ef460f1f3b61a69d5e7731ca1b20836ebf4a
Fthullh <fthullh99@gmail.com>
2021-05-29 22:12:42 +0800

    Input dengan Scanner

--- Module01/DataTypes.java
+++ Module01/DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args) {
+
+ Scanner cin = new Scanner(System.in);
 
  String namaDepan = "Fathulloh";
  String namaTengah = "Muhammad";
@@ -12,6 +15,23 @@ public class DataTypes{
  char nilaiAbjad = 'A';
  boolean tampan = true;
 
+ System.out.print("Input Nama Depan :");
+ namaDepan = cin.nextLine();
+ System.out.print("Input Nama Tengah :");
+ namaTengah = cin.nextLine();
+ System.out.print("Input Nama Belakang :");
+ namaBelakang = cin.nextLine();
+ System.out.print("Input Usia : ");
+ usia = cin.nextInt();
+ System.out.print("Input IPK: ");
+ ipk = cin.nextDouble();
+ System.out.print("");
+ System.out.print("Input Nilai Abjad : ");
+ nilaiAbjad = cin.next().charAt(0);
+ System.out.print("Tampan? : ");
+ tampan = cin.nextBoolean();
+
+ System.out.println("~~~~~~~~~~OUTPUT~~~~~~~~~~~");
  System.out.println("Nama depan : " + namaDepan);
  System.out.println("Nama tengah : " + namaTengah);
  System.out.println("Nama belakang: " + namaBelakang);
